b383f597d6971693f0a76b8f21a4e251cfc3a1df
Mike Fowler <mike.fowler@airbnb.com>
2017-05-24 14:42:43 -0700

    Wrap setup tasks in OSX check, as appropriate

--- setup.sh
+++ setup.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/env bash
 
-# Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# If we're on OSX…
+if [ "$(uname)" == "Darwin" ]; then
 
-# Homebrew
-brew update
-brew upgrade
+  # Install Homebrew
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install rbenv + Ruby, and then set it as the global default
-brew install rbenv
-rbenv install 2.4.1
-rbenv global 2.4.1
+  # Update and install upgrades for Homebrew
+  brew update
+  brew upgrade
 
-# Install Tmux and Tmuxinator.
-brew install tmux
-gem install tmuxinator
+  # Install rbenv + Ruby, and then set it as the global default
+  brew install rbenv
+  rbenv install 2.4.1
+  rbenv global 2.4.1
+
+  # Install Tmux and Tmuxinator.
+  brew install tmux
+  gem install tmuxinator
+
+  # Install Bash completion
+  brew install bash-completion
+
+fi
 
 # Install Tmux Plugin Manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# Install Bash completion
-brew install bash-completion
 
 # Install Vundle vim plugin manager and then install plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
